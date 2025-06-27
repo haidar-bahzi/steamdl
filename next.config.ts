34a994b4b76d9bb85dbd6c22eb527c92c8a525a8
Haidar Bahzi <realhaidarbahzi@proton.me>
2025-06-27 22:03:38 +0700

    again

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["steamuserimages-a.akamaihd.net"], // Add your domain here
+    domains: ["steamuserimages-a.akamaihd.net", "27ad-180-253-72-50.ngrok-free.app"], // Add your domain here
   },
 };
 
